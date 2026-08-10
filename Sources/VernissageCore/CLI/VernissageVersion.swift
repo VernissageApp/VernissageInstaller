fac5f52db5dd850c786ee4d76be5638e70c617cd
Marcin Czachurski <mczachurski@icloud.com>
2026-08-10 11:10:36 +0200

    Change version to 0.1.7

--- Sources/VernissageCore/CLI/VernissageVersion.swift
+++ Sources/VernissageCore/CLI/VernissageVersion.swift
@@ -1,4 +1,4 @@
 public enum VernissageVersion {
-    public static let current = "0.1.6"
+    public static let current = "0.1.7"
     public static let formatted = "vernissagectl \(current)"
 }
--- Tests/VernissageCoreTests/VernissageCommandTests.swift
+++ Tests/VernissageCoreTests/VernissageCommandTests.swift
@@ -5,8 +5,8 @@ import Testing
 struct VernissageCommandTests {
     @Test
     func `Current version is formatted`() {
-        #expect(VernissageVersion.current == "0.1.6")
-        #expect(VernissageVersion.formatted == "vernissagectl 0.1.6")
+        #expect(VernissageVersion.current == "0.1.7")
+        #expect(VernissageVersion.formatted == "vernissagectl 0.1.7")
     }
 
     @Test
